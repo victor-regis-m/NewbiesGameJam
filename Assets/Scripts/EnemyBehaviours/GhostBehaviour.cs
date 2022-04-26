76205797c8eaf0ee807f3dc96130c5c54f489970
Victor <victor.regism@gmail.com>
2022-04-25 22:23:06 -0700

    Added player attacks and configured enemy health system

--- Assets/Scripts/EnemyBehaviours/GhostBehaviour.cs
+++ Assets/Scripts/EnemyBehaviours/GhostBehaviour.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class GhostBehaviour : EnemyBase
 {
     float initialTime;
-    float moveSpeed;
     Vector3 initialPosition;
     void Start()
     {
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,9 +6,11 @@ public class EnemyController : MonoBehaviour
 {
     EnemyBase enemyActionsHandler;
     [SerializeField]EnemySO enemySO;
+    int cachedHealthPoints;
     void Awake()
     {
         SpriteRenderer sr = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+        cachedHealthPoints = enemySO.GetHealthPoints();
         sr.sprite = enemySO.GetEnemySprite();
         enemyActionsHandler = GetComponent<EnemyBase>();
         enemyActionsHandler.ParseMoveSpeed(enemySO.GetMoveSpeed());
@@ -32,7 +34,19 @@ public class EnemyController : MonoBehaviour
     {
         if(enemySO.IsAlive())
             enemyActionsHandler.Attack();
+        else
+            enemyActionsHandler.Die();
     }
 
-    
+    private void OnApplicationQuit() 
+    {
+        enemySO.ResetEnemyHealthPoints(cachedHealthPoints);
+    }
+    private void OnDestroy() 
+    {
+        enemySO.ResetEnemyHealthPoints(cachedHealthPoints);
+    }
+
+    public EnemyBase GetEnemyBase() => enemyActionsHandler;
+    public EnemySO GetEnemySO() => enemySO;
 }
